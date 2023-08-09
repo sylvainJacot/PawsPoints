6dbbc20dd0270f44205461968b0ea1666bcbc832
Sylvain <jct.sylvain@gmail.com>
2023-08-09 22:01:09 +0200

    removed login screen and wip sign up google btn

--- components/navigation/authStack.tsx
+++ components/navigation/authStack.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import WelcomeScreen from '../screens/Welcome';
-import SignInScreen from '../screens/SignInScreen';
 import SignOutScreen from '../screens/SignUpScreen';
 
 const Stack = createStackNavigator();
@@ -13,7 +12,6 @@ export default function AuthStack() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Sign In" component={SignInScreen} />
         <Stack.Screen name="Sign Up" component={SignOutScreen} />
       </Stack.Navigator>
     </NavigationContainer>

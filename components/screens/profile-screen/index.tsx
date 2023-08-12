12aaf8b4e0c9be986ac406c4569c595f1b9a2aed
Sylvain <jct.sylvain@gmail.com>
2023-08-12 21:39:57 +0200

    Some content on profile screen + switch pro/client mode

--- components/screens/profile-screen/index.tsx
+++ components/screens/profile-screen/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Types
 import { StackScreenProps } from '@react-navigation/stack'; 
@@ -7,15 +7,47 @@ import { StackNavigationParamList } from '../../navigation/type';
 // Component
 import ProfileScreenClient from './variants/client-mode';
 import ProfileScreenPro from './variants/pro-mode';
+import { TextInput, View } from 'react-native';
+import { Button, Switch, Text } from 'react-native-elements';
 
 type ProfileScreenNavigationProps = StackScreenProps<StackNavigationParamList, 'Profile'>;
 
 export default function ProfileScreen({ route }: ProfileScreenNavigationProps) {
 
-    const { isProMode, isClientMode } = route.params;
+    const { isProMode: initialIsProMode, isClientMode } = route.params;
+
+    const [isProMode, setIsProMode] = useState<boolean>(initialIsProMode);
+    const [name, setName] = useState<string>('');
+    const [firstName, setFirstName] = useState<string>('');
+    const [phoneNumber, setPhoneNumber] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+  
+
+    const handleSaveProfile = () => {
+      // Implement logic to update user profile data in the database
+    };
+  
+    const handleSwitchToProMode = () => {
+      // Implement logic to update user's mode in the database
+      setIsProMode(!isProMode);
+    };
 
   return (
     <React.Fragment>
+          <View>
+      <Text>Edit Profile</Text>
+      <TextInput value={name} onChangeText={setName} placeholder="Name" />
+      <TextInput value={firstName} onChangeText={setFirstName} placeholder="First Name" />
+      <TextInput value={phoneNumber} onChangeText={setPhoneNumber} placeholder="Phone Number" />
+      <TextInput value={email} onChangeText={setEmail} placeholder="Email" />
+      <TextInput value={password} onChangeText={setPassword} placeholder="Password" secureTextEntry />
+
+
+      <Button title="Save" onPress={handleSaveProfile} />
+      <Text>Change mode : </Text>
+      <Switch value={isProMode} onValueChange={handleSwitchToProMode} />
+    </View>
       {isProMode ? <ProfileScreenPro /> : null}
       {isClientMode ? <ProfileScreenClient /> : null}
     </React.Fragment>
--- components/screens/profile-screen/variants/pro-mode/index.tsx
+++ components/screens/profile-screen/variants/pro-mode/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TextInput, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function ProfileScreenPro() {
   return (
     <View>
-        ProfileScreenPro
-    </View>
+    <TextInput placeholder="Activity Name" />
+    <Button title="Edit Fidelity Card" onPress={() => {}} />
+  </View>
   );
 }
